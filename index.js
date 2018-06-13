0a43829a24815351a2fba7a9b7b9bbf3a4d75ba8
Jacob Spade <jacobspade@Jacobs-MacBook-Pro.local>
2018-06-13 08:11:20 -0700

    created routes to add skills

--- index.js
+++ index.js
@@ -16,13 +16,26 @@ app.get('/', function(req, res) {
 app.get('/skills', function(req, res) {
   var skills = fs.readFileSync('./data.json');
   skills = JSON.parse(skills);
+  console.log(skills);
   // TODO: Render this array of skills into an EJS template
-  res.send(skills);
+  res.render('skills', {skills: skills});
 });
 
 // TODO: Add GET route that returns static page containing a form
 
+app.get('/form', function(req, res) {
+  res.sendFile(__dirname + '/static/html/form.html');
+});
+
 // TODO: Add POST route that writes new skill to the file, redirects to skills index
 // TODO: Form should have action='/skills' and method='POST'
+
+app.post('/skills', function(req, res) {
+  var skills = fs.readFileSync('./data.json');
+  skills = JSON.parse(skills);
+  skills.push({name: req.body.name, level: req.body.level});
+  fs.writeFileSync('./data.json', JSON.stringify(skills));
+  res.redirect('/skills');
+});
 
 app.listen(process.env.PORT || 3000);
